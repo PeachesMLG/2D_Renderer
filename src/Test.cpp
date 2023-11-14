ece19ba3d1eabda045de2c0389c2de7cd4712e81
Peaches_MLG <PeachesMLG100@gmail.com>
2023-11-14 19:58:04 +0000

    boink

---
+++ src/Test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Test.h"
+
+void Test::Testing(TestStruct test) {
+    std::cout << test.x << std::endl;
+}
+
+Test::Test() {
+
+}
---
+++ src/Test.h
@@ -0,0 +1,17 @@
+#ifndef INC_2D_RENDERER_TEST_H
+#define INC_2D_RENDERER_TEST_H
+
+struct TestStruct {
+    int x;
+};
+
+
+class Test {
+public:
+    Test();
+
+    void Testing(TestStruct test);
+};
+
+
+#endif //INC_2D_RENDERER_TEST_H
